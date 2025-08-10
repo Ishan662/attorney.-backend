7ed43106396b54ff071034387bbb34e606828fe2
Nishagi <jeewanthadeherath04@gmail.com>
2025-08-10 20:46:59 +0530

    ATTO-64 created the lawyer model(entity) with court colors column

---
+++ backend/src/main/java/com/example/backend/model/lawyer/Lawyer.java
@@ -0,0 +1,43 @@
+package com.example.backend.model.lawyer;
+
+import com.example.backend.model.user.User;
+import com.vladmihalcea.hibernate.type.json.JsonNodeBinaryType;
+import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.Map;
+
+@Entity
+@Table(name="lawyers")
+public class Lawyer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "court_colors", columnDefinition = "jsonb")
+    private Map<String, String> courtColors;
+
+    public Lawyer(){}
+
+    public Lawyer(User user, Map<String,String> courtColors){
+        this.user = user;
+        this.courtColors = courtColors;
+    }
+
+    public Long getId() {return id;}
+    public void setId(Long id){this.id= id;}
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public Map<String, String> getCourtColors() { return courtColors; }
+    public void setCourtColors(Map<String, String> courtColors) { this.courtColors = courtColors; }
+
+}
