ef915c9780f81cfa9f6f1f175560c3d8d46fd4cb
Nishagi <jeewanthadeherath04@gmail.com>
2025-08-11 17:57:35 +0530

    created the lawyr controller

--- backend/src/main/java/com/example/backend/controllers/LawyerController.java
+++ backend/src/main/java/com/example/backend/controllers/LawyerController.java
@@ -1,6 +1,12 @@
 package com.example.backend.controllers;
 
 
+import com.example.backend.dto.lawyerDTOS.CourtColorsRequest;
+import com.example.backend.model.lawyer.Lawyer;
+import com.example.backend.service.LawyerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/lawyers")
 public class LawyerController {
 
-    private final
+    private final LawyerService lawyerService;
+
+    public LawyerController(LawyerService lawyerService){
+        this.lawyerService = lawyerService;
+    }
+
+    @PostMapping("/court-colors")
+    public ResponseEntity<Lawyer> saveCourtColors(@RequestBody CourtColorsRequest request){
+        Lawyer lawyer = lawyerService.saveOrUpdateCourtColors(request);
+        return ResponseEntity.ok(lawyer);
+    }
 }
