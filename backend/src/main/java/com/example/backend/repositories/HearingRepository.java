c11f4707fc93b9f2fdeba4a385e726c8cc81761d
Ishan maduranga <ishanmadurangai662@gmail.com>
2025-09-06 15:25:13 +0530

    feature/ATTO-65 some changes saved in hearing repository

--- backend/src/main/java/com/example/backend/repositories/HearingRepository.java
+++ backend/src/main/java/com/example/backend/repositories/HearingRepository.java
@@ -10,11 +10,6 @@ import java.util.UUID;
 
 @Repository
 public interface HearingRepository extends JpaRepository<Hearing, UUID> {
-
-    // This method name tells Spring Data JPA to:
-    // "Find all Hearing entities...
-    // where the 'aCase' object's 'id' field matches the given caseId...
-    // and order the results by the 'hearingDate' field in ascending order."
     List<Hearing> findAllByaCase_IdOrderByHearingDateAsc(UUID caseId);
 
     List<Hearing> findByaCase_IdOrderByHearingDateAsc(UUID id);
